b4815afa0d7c27847ba6d02ed4fd44d776a05afc
Pedro Costa <pedro.c.costa@pdmfc.com>
2021-04-23 11:46:36 -0300

    refs #4 - add acdcuser service

--- backoffice-backend/src/acdc/acdc.module.ts
+++ backoffice-backend/src/acdc/acdc.module.ts
@@ -7,13 +7,14 @@ import {EventController} from "./event.controller";
 import {EventRepository} from "./event.repository";
 import {EventInputRepository} from "./eventinput.repository";
 import {EventOutputRepository} from "./eventoutput.repository";
+import {AcdcUserService} from "./acdcuser.service";
 
 
 @Module({
     imports: [TypeOrmModule.forRoot()],
     controllers: [AppResourceController, LocaleController, EventController],
-    providers: [AppUserService, EventRepository, EventInputRepository, EventOutputRepository],
-    exports: [AppUserService, EventRepository, EventInputRepository, EventOutputRepository],
+    providers: [AppUserService, EventRepository, EventInputRepository, EventOutputRepository, AcdcUserService],
+    exports: [AppUserService, EventRepository, EventInputRepository, EventOutputRepository, AcdcUserService],
 })
 export class AcdcModule {
 }
