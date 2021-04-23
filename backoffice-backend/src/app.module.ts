3525293532798094dffd9e0ef497b5730c86f6db
Pedro Costa <pedro.c.costa@pdmfc.com>
2021-04-23 11:40:26 -0300

    refs #4 - add scan module

--- backoffice-backend/src/app.module.ts
+++ backoffice-backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AuthModule } from './auth/auth.module';
+import {ScanModule} from "./scan/scan.module";
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), AcdcModule, AuthModule],
+  imports: [TypeOrmModule.forRoot(), AcdcModule, AuthModule, ScanModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backoffice-backend/src/scan/scan.controller.ts
@@ -0,0 +1,29 @@
+import {Body, Controller, Post} from "@nestjs/common";
+import {ApiOkResponse, ApiOperation, ApiTags} from "@nestjs/swagger";
+import {ScanService} from "./scan.service";
+import {EventInputDataDto} from "../acdc/eventinput.dto";
+import {EventOuputDataDto} from "../acdc/eventoutput.dto";
+
+@ApiTags("Scan")
+@Controller("/scan")
+export class ScanController {
+    constructor(private scanService: ScanService) {
+    }
+
+    @Post()
+    @ApiOperation({summary: "Create one Scan"})
+    @ApiOkResponse({
+        description: "Data are processed, and the authentication status of the scan is returned. \n\n*For demo purpose, the currently returned data is dummy.",
+        schema: {
+            type: "object",
+            properties: {
+                SN_check_result: {type: 'string'},
+                MAH_ID: {type: 'string'},
+            }
+        },
+    })
+    async create(@Body() eventInputData: EventInputDataDto): Promise<EventOuputDataDto> {
+        console.log("/scan... create.scan=", eventInputData);
+        return await this.scanService.create(eventInputData)
+    }
+}
