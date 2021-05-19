092dc629597b90971a0f3ac214d445ef8967f61e
Joao Luis <joao.luis@pdmfc.com>
2021-05-19 20:26:34 +0100

    #4 - LoggedInGuard redirects to /login when not logged in

--- backoffice-frontend/src/app/auth/logged-in.guard.ts
+++ backoffice-frontend/src/app/auth/logged-in.guard.ts
@@ -1,13 +1,19 @@
 // based on https://itnext.io/handle-restricted-routes-in-angular-with-route-guards-95c93be9d05e
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 
 @Injectable()
 export class LoggedInGuard implements CanActivate {
     constructor(private authService: AuthService, private router: Router) { }
-    canActivate(): Observable<boolean> | Promise<boolean> | boolean {
-        return this.authService.isLoggedIn();
+    
+    canActivate(
+        next: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot
+    ): true|UrlTree {
+        if ( this.authService.isLoggedIn() )
+            return true;
+        return this.router.parseUrl('/login');
     }
 }
