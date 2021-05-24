68ce8f3ea1f5c5e9f15417076b05b066fa5173bd
Pedro Costa <pedro.c.costa@pdmfc.com>
2021-05-24 07:40:36 -0300

    refs #4 - add users and remove appuser table in acdc db/schema

--- backoffice-backend/src/acdc/acdc.module.ts
+++ backoffice-backend/src/acdc/acdc.module.ts
@@ -2,7 +2,6 @@ import {Module} from '@nestjs/common';
 import {TypeOrmModule} from '@nestjs/typeorm';
 import {AppResourceController} from './appresource.controller';
 import {LocaleController} from './locale.controller';
-import {AppUserService} from './appuser.service';
 import {EventController} from "./event.controller";
 import {EventRepository} from "./event.repository";
 import {EventInputRepository} from "./eventinput.repository";
@@ -13,8 +12,8 @@ import {AcdcUserService} from "./acdcuser.service";
 @Module({
     imports: [TypeOrmModule.forRoot()],
     controllers: [AppResourceController, LocaleController, EventController],
-    providers: [AppUserService, EventRepository, EventInputRepository, EventOutputRepository, AcdcUserService],
-    exports: [AppUserService, EventRepository, EventInputRepository, EventOutputRepository, AcdcUserService],
+    providers: [EventRepository, EventInputRepository, EventOutputRepository, AcdcUserService],
+    exports: [EventRepository, EventInputRepository, EventOutputRepository, AcdcUserService],
 })
 export class AcdcModule {
 }
