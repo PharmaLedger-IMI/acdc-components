304b7df440570e199e6f2faceb34af94b7fcca72
t <t>
2021-09-06 00:01:39 +0100

    refs #11 - update definint THREE lib for control compatibility

--- native-integration/src/ios/lib/lib.js
+++ native-integration/src/ios/lib/lib.js
@@ -1,4 +1,4 @@
-const THREE = require('../../../node_modules/three/build/three');
+globalThis.THREE = require('../../../node_modules/three/build/three');
 require('../../../node_modules/three/examples/js/controls/OrbitControls');
 module.exports = {
     THREE
