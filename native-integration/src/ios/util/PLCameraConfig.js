c4ebb4d30f3e2a7d24da810a31c08c003c83c2b0
t <t>
2021-10-07 11:56:03 +0100

    refs #12 - includes initOrientation property && default value

--- native-integration/src/ios/util/PLCameraConfig.js
+++ native-integration/src/ios/util/PLCameraConfig.js
@@ -12,8 +12,9 @@ class PLCameraConfig {
      * @param  {string | undefined} preferredColorSpace=undefined Possible values are "sRGB", "P3_D65" or "HLG_BT2020".
      * @param  {number} torchLevel=1.0 Float in the range of 0 to 1.0
      * @param  {number} aspectRatio=4.0/3.0 This value will not be used
+     * @param  {string} initOrientation="portrait" Predefines the orientation when initializing the camera (available values are "landscapeRight", "landscapeLeft" and "portrait").
      */
-    constructor(sessionPreset, flashConfiguration = "auto", continuousFocus = true, autoOrientationEnabled = true, deviceTypes = ["wideAngleCamera"], cameraPosition = "back", highResolutionCaptureEnabled = true, preferredColorSpace = undefined, torchLevel = 1.0, aspectRatio = 4.0 / 3.0) {
+    constructor(sessionPreset, flashConfiguration = "auto", continuousFocus = true, autoOrientationEnabled = true, deviceTypes = ["wideAngleCamera"], cameraPosition = "back", highResolutionCaptureEnabled = true, preferredColorSpace = undefined, torchLevel = 1.0, aspectRatio = 4.0/3.0, initOrientation = "portrait") {
         this.sessionPreset = sessionPreset;
         this.flashConfiguration = flashConfiguration;
         this.torchLevel = torchLevel;
@@ -24,6 +25,7 @@ class PLCameraConfig {
         this.highResolutionCaptureEnabled = highResolutionCaptureEnabled;
         this.preferredColorSpace = preferredColorSpace;
         this.aspectRatio = aspectRatio;
+        this.initOrientation = initOrientation;
     }
 }
 
